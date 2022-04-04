49a5126d7a17b33d864df8e018d0afbdbf66abfe
John Gardner <gardner.john97+dphil@gmail.com>
2022-04-04 11:26:59 +0100

    typing

--- book_keeping/util.py
+++ book_keeping/util.py
@@ -1,3 +1,8 @@
+import os
+from pathlib import Path
+from typing import Generator
+
+
 def dual_filter(predicate, iterable):
     yes = [*filter(predicate, iterable)]
     no = [e for e in iterable if e not in yes]
@@ -24,3 +29,10 @@ def apply(func, iterable):
 
 def is_img(path):
     return path.suffix in (".svg", ".png", ".jpeg")
+
+
+def all_files(p: os.PathLike) -> Generator[Path, None, None]:
+    for path, _, files in os.walk(p):
+        rel_path = Path(path).relative_to(p)
+        for name in files:
+            yield rel_path / name
